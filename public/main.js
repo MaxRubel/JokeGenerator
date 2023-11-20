a7e13a1e6d196da109a447be7d88816629b1827e
MaxRubel <maxrubel1@gmail.com>
2023-11-20 16:19:01 -0600

    tried some things

--- public/main.js
+++ public/main.js
@@ -10,7 +10,7 @@ const init = () => {
   let setup;
   let punchLine;
   let i = 0;
-
+  let dataArray;
   const renderDom = () => {
     app.innerHTML = `        
     <h2 id="jokeBody"></h2>
@@ -35,33 +35,42 @@ const init = () => {
       .then((response) => response.json())
       .then((data) => {
         resolve(data);
+        dataArray = data;
         setup = data.setup;
         punchLine = data.delivery;
       })
       .catch(reject);
   });
 
+  // getRequest();
   getRequest();
 
   const JokeSequence = () => {
     if (i === 1) {
+      console.warn(i);
       jokeBody.innerHTML = setup;
       jokeButton.innerHTML = 'Get the Punchline.';
     }
     if (i === 2) {
+      console.warn(i);
       punchLineBody.innerHTML = punchLine;
       jokeButton.innerHTML = 'Lol. Hear another one?';
     }
     if (i === 3) {
+      console.warn(i);
+      getRequest();
       i = 0;
+      setup = '';
+      punchLine = '';
       jokeBody.innerHTML = '';
       punchLineBody.innerHTML = '';
       jokeButton.innerHTML = 'Click me to hear a joke';
-      getRequest();
+      console.warn(i);
     }
   };
 
   document.getElementById('jokeLol').addEventListener('click', () => {
+    console.warn(dataArray);
     i += 1;
     JokeSequence();
   });
